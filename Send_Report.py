70f5fdf7cf5a6e68eb153279d708b02a93efc356
Victória Regina Duarte <victoriarduarte@hotmail.com>
2022-09-15 12:42:08 -0300

    Adiciona argumento 'emails_to'

--- Send_Report.py
+++ Send_Report.py
@@ -10,13 +10,12 @@ import sys
 from jenkinsapi.jenkins import Jenkins
 
 def mail_report_deploy_mocks(job):
-
     host = "smtp.gmail.com"
     port = "587"
     email_from = "vique.teste@gmail.com"
     email_from_alias = "Equipe QA"
     password = "twgbocwzstjzjjyx"
-    email_to = "victoriarduarte@hotmail.com;victoria.duarte@ritain.io"
+    # email_to = "victoriarduarte@hotmail.com;victoria.duarte@ritain.io;fernandoleite@live.com"
     email_subject = subject_email
 
     job_enviar_email = job
@@ -36,22 +35,21 @@ def mail_report_deploy_mocks(job):
     </html>
     """.format(job_enviar_email=job_enviar_email, last_completed_build_url=last_completed_build_url)
 
-
     email_msg = MIMEMultipart()
     email_msg['From'] = formataddr((str(Header(email_from_alias, 'utf-8')), email_from))
-    email_msg['To'] = email_to
+    email_msg['To'] = emails_to
     email_msg['Subject'] = email_subject
     email_msg.attach(MIMEText(email_body, 'html', 'UTF-8'))
 
-    print("A enviar email para " + email_to)
+    print("Enviar email para: " + str(emails_to))
 
-    server.sendmail(email_from,email_to.split(';'),email_msg.as_string())
+    server.sendmail(email_from,emails_to.split(';'),email_msg.as_string())
     print("Email enviado!")
 
     server.quit()
 
 
-def obter_info_testset(jenkins_url, jenkins_user, jenkins_pass, view, folder, job):
+def obter_info_job(jenkins_url, jenkins_user, jenkins_pass, view, folder, job):
     """
     :param jenkins_url: String com Jenkins endpoint
     :param jenkins_user: String com Jenkins User
@@ -70,40 +68,40 @@ def obter_info_testset(jenkins_url, jenkins_user, jenkins_pass, view, folder, jo
     print "#######################################################"
 
     # Info relativa ao TestSet
-    testset_response = Jenkins_Client.get_data(
+    job_response = Jenkins_Client.get_data(
         jenkins_url + "view/" + view + "/job/" + folder + "/job/" + job + "/api/python")
 
     print ""
-    print "::: TESTSET INFO :::"
+    print "::: JOB INFO :::"
 
-    global nome_build
-    nome_build = testset_response['fullDisplayName']
-    print nome_build
+    global nome_job
+    nome_job = job_response['fullDisplayName']
+    print nome_job
     # Running
     global last_completed_build_url
-    last_completed_build_url = testset_response['lastCompletedBuild']['url']
+    last_completed_build_url = job_response['lastCompletedBuild']['url']
     global last_completed_build_n_str
-    last_completed_build_number = testset_response['lastCompletedBuild']['number']
+    last_completed_build_number = job_response['lastCompletedBuild']['number']
     last_completed_build_n_str = str(last_completed_build_number)
 
     print "last_completed_build_url: " + last_completed_build_url
-    print "lastCompletedBuild NUMBER: " + last_completed_build_n_str
+    print "last_completed_build_number: " + last_completed_build_n_str
 
-    print "::: TESTSET INFO :::"
+    print "::: JOB INFO :::"
     print ""
-    print "XXXXXXXXXXXXX"
+    print "::: TESTSET BUILD INFO :::"
     print last_completed_build_url
 
     # Info relativa a ultima build do TestSet
-    testset_build_response = Jenkins_Client.get_data(last_completed_build_url + "/api/python")
+    last_build_response = Jenkins_Client.get_data(last_completed_build_url + "/api/python")
 
-    print "testset_build_response:"
-    print testset_build_response
+    print "last_build_response:"
+    print last_build_response
+
+    global last_build_result
+    last_build_result = last_build_response['result']
+    print 'last_build_result: ' + last_build_result
     print "::: TESTSET BUILD INFO :::"
-
-    global testset_b_result
-    testset_b_result = testset_build_response['result']
-    print 'testset_b_result: ' + testset_b_result
 
 
 ##########################################
@@ -126,18 +124,16 @@ if __name__ == "__main__":
     # view = "Deploys"
     # folder = "Mocks"
     # job = "Restart Mocks 10"
-    view = sys.argv[1]
-    folder = sys.argv[2]
-    job = sys.argv[3]
+    # emails_to = "victoriarduarte@hotmail.com;victoria.duarte@ritain.io"
 
-    len_args = len(sys.argv)
-    print("len_args:" + str(len_args))
+    emails_to = sys.argv[1]
+    view = sys.argv[2]
+    folder = sys.argv[3]
+    job = sys.argv[4]
 
-    global subject_email
     subject_email = job + " - Report"
 
-    # subject_email = folder + " - Report"
-    # emails_to = emails_to.replace(";", ",")
+    emails_to = emails_to.replace(",", ";")
 
     print("")
     print("--------------------------------")
@@ -147,19 +143,19 @@ if __name__ == "__main__":
     print("--------------------------------")
     print("------ report email -------")
     print("--------------------------------")
-    # print("--emails_to: " + str(emails_to))
+    print("--emails_to: " + str(emails_to))
     print("----subject: " + str(subject_email))
-    print("--view: " + str(view))
-    print("---folder: " + str(folder))
-    print("---job: " + str(job))
-    print("--------------------------")
+    print("-------view: " + str(view))
+    print("-----folder: " + str(folder))
+    print("--------job: " + str(job))
+    print("--------------------------------")
     print("")
 
-    lista_testes = obter_info_testset(jenkins_url, user, passwd, view, folder, job)
+    obter_info_job(jenkins_url, user, passwd, view, folder, job)
 
-    if testset_b_result != "SUCCESS":
-        print("Finished: FAILURE")
+    if last_build_result != "SUCCESS":
+        print("\nBuild status: FAILURE")
         mail_report_deploy_mocks(job)
     else:
-        print("Finished: SUCCESS")
+        print("\nBuild status: SUCCESS")
         print("E-mail de reportt não enviado")
